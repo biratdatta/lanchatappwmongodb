684776b049468558cedaed8f0319e9f1501724d5
biratdatta <birat.datta26@gmail.com>
2024-04-06 22:39:13 +0530

    quality of life changes

--- frontend/src/pages/signup/GenderCheckbox.jsx
+++ frontend/src/pages/signup/GenderCheckbox.jsx
@@ -3,7 +3,7 @@ const GenderCheckbox = ({ onCheckboxChange, selectedGender }) => {
 		<div className='flex'>
 			<div className='form-control'>
 				<label className={`label gap-2 cursor-pointer ${selectedGender === "male" ? "selected" : ""} `}>
-					<span className='label-text'>Male</span>
+					<span className='label-text text-black'>Male</span>
 					<input
 						type='checkbox'
 						className='checkbox border-slate-900'
@@ -13,8 +13,8 @@ const GenderCheckbox = ({ onCheckboxChange, selectedGender }) => {
 				</label>
 			</div>
 			<div className='form-control'>
-				<label className={`label gap-2 cursor-pointer  ${selectedGender === "female" ? "selected" : ""}`}>
-					<span className='label-text'>Female</span>
+				<label className={`label gap-2 cursor-pointer   ${selectedGender === "female" ? "selected" : ""}`}>
+					<span className='label-text text-black'>Female</span>
 					<input
 						type='checkbox'
 						className='checkbox border-slate-900'
@@ -27,24 +27,3 @@ const GenderCheckbox = ({ onCheckboxChange, selectedGender }) => {
 	);
 };
 export default GenderCheckbox;
-
-// STARTER CODE FOR THIS FILE
-// const GenderCheckbox = () => {
-// 	return (
-// 		<div className='flex'>
-// 			<div className='form-control'>
-// 				<label className={`label gap-2 cursor-pointer`}>
-// 					<span className='label-text'>Male</span>
-// 					<input type='checkbox' className='checkbox border-slate-900' />
-// 				</label>
-// 			</div>
-// 			<div className='form-control'>
-// 				<label className={`label gap-2 cursor-pointer`}>
-// 					<span className='label-text'>Female</span>
-// 					<input type='checkbox' className='checkbox border-slate-900' />
-// 				</label>
-// 			</div>
-// 		</div>
-// 	);
-// };
-// export default GenderCheckbox;
